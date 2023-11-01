fe3437eb8c162286d16da68e7821dfa972cbdcf3
gmb sumber <git.gmb@gmail.com>
2023-11-01 11:03:34 +0700

    tambah

--- lib/common/global_variable.dart
+++ lib/common/global_variable.dart
@@ -1,4 +1,3 @@
 class GlobalVariables {
-  //static const baseUrl = 'https://0649-180-253-161-18.ngrok-free.app';
-  static const baseUrl = 'https://642f-202-51-122-252.ngrok-free.app';
+  static const baseUrl = 'https://c014-202-51-122-252.ngrok-free.app';
 }
